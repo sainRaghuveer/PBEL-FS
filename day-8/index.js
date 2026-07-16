b877bc4283f626a28e1511bd9b8c96ca2655eceb
sainRaghuveer <raghuveersain987@gmail.com>
2026-07-16 17:41:02 +0530

    day-8

---
+++ day-8/index.js
@@ -0,0 +1,113 @@
+// // let, const, var
+
+// let firstName = "John";
+// let lastName = "Doe";
+// console.log(firstName+ " "+ lastName)   //Concatenation
+
+
+// console.log(typeof name)
+
+// let num = 20;
+// let num2 = 40;
+
+// let sum = num+num2
+// console.log(sum)
+
+// console.log(typeof num);
+
+// let num1 = 5.6
+
+// console.log(typeof num1)
+
+// let bool = true
+
+// console.log(typeof bool)
+
+
+// equal == / ===, not equal !=/ !==, less than <, greater than >, less than equal <=, greater than equal >=
+
+
+let num1 = 20;
+let num2 = 20;
+
+// console.log(num1<=num2)
+
+
+// if, else, else if
+
+
+// if(num1<num2){
+//     console.log("I won...")
+// }else{
+//     console.log("You loss...")
+// }
+
+
+
+// if(num1<num2){
+//     console.log("I won...")
+// }else if(num1>num2){
+//     console.log("You loss...")
+// }else if(num1==num2){
+//     console.log("You are looser")
+// }else{
+//     console.log("I am winner")
+// }
+
+
+
+// console.log(typeof undefined)
+
+
+// console.log(null === undefined)
+
+
+let arr = [1, "Name", 3.6, true, false, null, undefined, [2,4,5]];
+
+let lastIndex = arr.length-1;
+
+console.log(lastIndex)
+
+console.log(arr.length)
+
+arr.push(6);
+
+
+console.log(arr)
+
+arr.pop();
+
+console.log(arr);
+
+console.log(arr[arr.length-1]);
+
+arr[3]= 18;
+
+console.log(arr);
+
+arr[0]="Myname Is name";
+
+
+console.log(arr);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// String, Number, Boolean, Object, Array, Function, Null, Undefined
+
+
+
