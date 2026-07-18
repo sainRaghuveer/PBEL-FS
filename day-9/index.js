5c419525b7c1334729c14ffae6b61da3a92ccbf8
sainRaghuveer <raghuveersain987@gmail.com>
2026-07-18 23:12:41 +0530

    PBEl

--- day-9/index.js
+++ day-9/index.js
@@ -1,89 +1,111 @@
-let obj = {
-    name: "Suraj",
-    uni: "ADTU",
-    course: "Btech",
-    year: 3,
-    isAlumni: false,
-    sub: ["FS", "AI", "DL", "ML"],
-    hobbies: {
-        games: ["Cricket", "Football"],
-        listening: "Starboy",
-        test:{
-            test1:{
-                test2:{
-                    test3:{
-                        test4:{
-                            test5:{
-                                abc:"RS"
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    },
-    67:"test"
-}
-
-let firstValue = obj["name"]
-
-console.log(firstValue);
-
-obj["course"] = "MCA";
-
-console.log(obj);
-
-let deletedKetValue = delete obj["uni"]
-
-console.log(deletedKetValue)
-
-console.log(obj.hobbies);
-
-obj["isMarried"]=false;
-
-
-console.log(obj.hobbies.test.test1.test2.test3.test4.test5["100"]);
-
-// obj["isMarried"]=true;
-
-// console.log(obj)
-
-
-
-// // loops:- for loop, while loop, for of loop, for in loop.
-
-// let num = 10;
-
-// for(let i = 0; i<num; i=i+1){
-//     console.log(i)
+// let obj = {
+//     name: "Suraj",
+//     uni: "ADTU",
+//     course: "Btech",
+//     year: 3,
+//     isAlumni: false,
+//     sub: ["FS", "AI", "DL", "ML"],
+//     hobbies: {
+//         games: ["Cricket", "Football"],
+//         listening: "Starboy",
+//         test:{
+//             test1:{
+//                 test2:{
+//                     test3:{
+//                         test4:{
+//                             test5:{
+//                                 abc:"RS"
+//                             }
+//                         }
+//                     }
+//                 }
+//             }
+//         }
+//     },
+//     67:"test"
 // }
 
+// let firstValue = obj["name"]
 
-// let std = ["Nishita", "Rezwan", "Rabi", "Aryan", "Mriganka", "Supriya", "Mahavir", "Nantelo","Bhaskar"]
+// console.log(firstValue);
+
+// obj["course"] = "MCA";
+
+// console.log(obj);
+
+// let deletedKetValue = delete obj["uni"]
+
+// console.log(deletedKetValue)
+
+// console.log(obj.hobbies);
+
+// obj["isMarried"]=false;
 
 
-// for(let i = 0; i<std.length; i++){
-//     for(let j=0; j<std[i].length; j++){
-//         console.log(std[i][j])
+// console.log(obj.hobbies.test.test1.test2.test3.test4.test5["100"]);
+
+// // obj["isMarried"]=true;
+
+// // console.log(obj)
+
+
+
+// // // loops:- for loop, while loop, for of loop, for in loop.
+
+// // let num = 10;
+
+// // for(let i = 0; i<num; i=i+1){
+// //     console.log(i)
+// // }
+
+
+// // let std = ["Nishita", "Rezwan", "Rabi", "Aryan", "Mriganka", "Supriya", "Mahavir", "Nantelo","Bhaskar"]
+
+
+// // for(let i = 0; i<std.length; i++){
+// //     for(let j=0; j<std[i].length; j++){
+// //         console.log(std[i][j])
+// //     }
+// // }
+
+
+// let alph = "abcdefghijklmnopqrstuvwxyz";
+
+// for (let i=0; i<alph.length; i++){
+//     if(i%2==0){
+//         console.log("This Alphabet", alph[i], "on even index" );
+//         if(i==10){
+//             console.log("This alphabet is not special", alph[i])
+//         }
+//     }else{
+//         console.log("This Alphabet", alph[i], "on odd index" );
+//         if(i==17){
+//             console.log("This alphabet is not special", alph[i])
+//         }
 //     }
 // }
 
 
-let alph = "abcdefghijklmnopqrstuvwxyz";
+// You have to take one input as number, if that n umber is multiple of 3 then print "Humpty", if the number is multiple of 5 the  print "dumpty", if number is multiple of both 3 and 5 then print "Humpty Dumpty"
 
-for (let i=0; i<alph.length; i++){
-    if(i%2==0){
-        console.log("This Alphabet", alph[i], "on even index" );
-        if(i==10){
-            console.log("This alphabet is not special", alph[i])
-        }
-    }else{
-        console.log("This Alphabet", alph[i], "on odd index" );
-        if(i==17){
-            console.log("This alphabet is not special", alph[i])
-        }
-    }
-}
+// You have one string which is str = "We are in full stack batch", in this string you need to get all vowels and make one word out of it. then print it.
+
+// output = eaeiuaa
+
+//and operator &&, or operator ||
+
+// let str = "We arE in full stack batch"
+
+// let out = "";
+
+// for(let i=0; i<str.length; i++){
+//     if(str[i]=="a" || str[i]=="e" || str[i]=="i"|| str[i]=="o" || str[i]=="u"){
+//         out+=str[i];
+//         // out = out+str[i]
+//     }
+// }
+
+// console.log(out)
 
 
 
@@ -92,3 +114,108 @@ for (let i=0; i<alph.length; i++){
 // for(let i=0; i<firstStudent.length; i++){
 //     console.log(firstStudent[i])
 // }
+
+
+// let num = 1;
+
+// while(num<10){
+//     console.log("while loop is running");
+
+//     num++;
+// }
+
+
+//for of loop is specially for arrays only
+
+
+// let arr = [1,2,3,4,5,6,7,8,9];
+
+// for(let num of arr){
+//     console.log(num)
+// }
+
+// let arr1 = ["Bikash", "Satyam", "Dhiman", "Satya", "Dikshyan"];
+
+
+// for (let names of arr1){
+//     if(names == "Satyam"){
+//         console.log("He is from ADTU")
+//     }else{
+//         console.log("He is from Amity")
+//     }
+// }
+
+
+//for in loop, this loop is specially used in objects
+
+
+// let obj = {
+//     name: "Suraj",
+//     uni: "ADTU",
+//     course: "Btech",
+//     year: 3,
+//     isAlumni: false,
+//     sub: ["FS", "AI", "DL", "ML"],
+//     hobbies: {
+//         games: ["Cricket", "Football"],
+//         listening: "Starboy",
+//     },
+//     67:"test"
+// }
+
+// //Find which subject has maximum marks along with subject name
+
+// let obj1 = {
+//     maths:56,
+//     english:89,
+//     science:67,
+//     punjabi:88
+// }
+
+// let max = 0
+// let sub = ""
+
+// for (let i in obj1){
+//     if(obj1[i]>max){
+//         max = obj1[i];
+//         sub = i
+//     }
+// }
+
+// console.log(sub, max)
+
+
+//functions
+
+
+
+function myFunction(a, b, c) {   //parameters
+    let out = a + b + c;
+
+    console.log(out)
+}
+
+//ES6:- Ecma script 6, commonJS
+
+// let, const, arrow function
+
+const arrowFun = (a, b, c) => {
+    let out = a + b + c;
+
+    console.log(out)
+}
+
+arrowFun(89, 89, 89)
+
+// arrowFun(10, 20, 30)   //invoking the function
+// arrowFun(10, 60, 40)
+
+// arrowFun(178, 567, 4563)
+
+// arrowFun(78654, 87634, 976345) //arguments
+
+
+console.log(batch)
+
+var batch = "FS";
+
