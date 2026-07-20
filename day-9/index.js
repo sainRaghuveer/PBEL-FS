15087d1ea1adb480efd1329bddd0d0b8dba69429
sainRaghuveer <raghuveersain987@gmail.com>
2026-07-20 22:25:19 +0530

    day-12

--- day-9/index.js
+++ day-9/index.js
@@ -189,23 +189,23 @@
 
 
 
-function myFunction(a, b, c) {   //parameters
-    let out = a + b + c;
+// function myFunction(a, b, c) {   //parameters
+//     let out = a + b + c;
 
-    console.log(out)
-}
+//     console.log(out)
+// }
 
 //ES6:- Ecma script 6, commonJS
 
 // let, const, arrow function
 
-const arrowFun = (a, b, c) => {
-    let out = a + b + c;
+// const arrowFun = (a, b, c) => {
+//     let out = a + b + c;
 
-    console.log(out)
-}
+//     console.log(out)
+// }
 
-arrowFun(89, 89, 89)
+// arrowFun(89, 89, 89)
 
 // arrowFun(10, 20, 30)   //invoking the function
 // arrowFun(10, 60, 40)
@@ -215,7 +215,124 @@ arrowFun(89, 89, 89)
 // arrowFun(78654, 87634, 976345) //arguments
 
 
-console.log(batch)
+// console.log(batch)
 
-var batch = "FS";
+// var batch = "FS";
 
+
+// let out = myFun(23, 45, 56);
+
+// console.log(out)
+
+
+// function myFun(){
+
+//     let sum = 100
+
+//     console.log(sum)
+
+// }
+
+// myFun()
+
+// console.log(sum)
+
+
+// console.log(name)
+
+// var name = "Praljyoti"
+
+
+// block scop, global scop
+
+// console.log(myName)             //myName = undefined
+
+// let myName = "Rabi"             //myName="Assam"
+
+// myName = "Assam"
+
+// console.log(myName)
+
+//IIFE
+
+// (()=>{
+//     console.log("IIFE")
+// })()
+
+
+//JS property Hoisting, all traditional functions, var
+//Execution context
+
+//Higher order function
+
+
+//forEach, map, filter, reduce
+
+//They all will have arrays as input
+
+// let arr = [1,2,3,4,5,6,7,8,9]
+
+
+//forEach
+
+// arr.forEach((e, i)=>{
+//     console.log(e*4)
+// })
+
+
+// //map
+
+// let mapOut = arr.map((e, i)=>{
+//     return e*2
+// })
+
+// console.log(mapOut)
+
+//filter
+
+// let filterOut = arr.filter((e, i)=>{
+//     return e%2!==0
+// })
+
+// console.log(filterOut)
+
+
+
+
+// console.log(reduceOut)
+
+
+// let arr1 = [2,4,6,8,10,3,5,7,9]
+
+
+// let out = arr.map((el, i)=>{
+//     return el*3
+// }).filter((el, i)=>{
+//     return el%5==0
+// }).forEach((el, i)=>{
+//     console.log(el*200)
+// })
+
+// console.log(out)
+
+
+// let arr = [1,2,3,4,5]
+
+// let out = arr.reduce((acc, el)=>{
+//     return acc+el
+// })
+
+// console.log(out)
+
+
+//You will be having one object of products with product name and product price, you need to find out final price of each product after certain discount.
+
+let products = {
+    laptop:45000,
+    iphone:67000,
+    mobileCover:1200,
+    tablet:58999
+}
+
+// discount is 15% on each product
+//Find out final price of each products after discount in certain relevant data structure
