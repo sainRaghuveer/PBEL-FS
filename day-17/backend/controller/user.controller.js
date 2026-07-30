559c0c219f39dfd79a31749243ef7af8f48e7f14
sainRaghuveer <raghuveersain987@gmail.com>
2026-07-30 19:13:35 +0530

    day-19

--- day-17/backend/controller/user.controller.js
+++ day-17/backend/controller/user.controller.js
@@ -1,24 +1,38 @@
 const { userModel } = require("../model/user.model");
+const bcrypt = require("bcrypt");
 
-const registration = async(req, res) =>{
+const registration = async (req, res) => {
 
-    const {fullName, email, password, phoneNumber} = req.body;
+    const { fullName, email, password, phoneNumber } = req.body;
 
-    if(fullName == "" || email == "" || password == "" || phoneNumber==""){
-        return res.send({"message":"All fields are required"});
+    if (fullName == "" || email == "" || password == "" || phoneNumber == "") {
+        return res.send({ "message": "All fields are required" });
+    }
+
+    const existUser = userModel.findOne({email});
+
+    if(existUser){
+        return res.status(400).send({"message":"User already exist"})
     }
 
     try {
-        const user = new userModel({fullName, email, password, phoneNumber});
-        await user.save();
+        bcrypt.hash(password, 5,  async function (err, hash) {
+            if (err) {
+                return res.send({ "message": "There is error while creating account" })
+            } else {
+                const user = new userModel({ fullName, email, password:hash, phoneNumber });
+                await user.save();
 
-        res.status(200).send({"message":"Registration is completed"})
+                res.status(200).send({ "message": "Registration is completed" })
+            }
+        });
+
     } catch (error) {
-        res.status(400).send({"message":error})
+        res.status(400).send({ "message": error })
     }
 }
 
 
-module.exports={
+module.exports = {
     registration
 }
