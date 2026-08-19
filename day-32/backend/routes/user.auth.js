ad3160edd06a71e19dac9314e0d23ab7a39b89d6
sainRaghuveer <raghuveersain987@gmail.com>
2026-08-19 19:11:28 +0530

    google auth

--- day-32/backend/routes/user.auth.js
+++ day-32/backend/routes/user.auth.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
 const passport = require("passport");
 const userRouter = express.Router();
 
@@ -6,13 +7,20 @@ userRouter.get("/api/google", passport.authenticate("google", { scope: ["profile
 
 userRouter.get(
   "/api/google/callback",
-  passport.authenticate("google", { failureRedirect: "http://localhost:5173/" }),
+  passport.authenticate("google", { session: false }),
   (req, res) => {
-    res.redirect("http://localhost:5173/home");
+    const token = jwt.sign({
+      id: req.user._id,
+      name: req.user.name,
+      email: req.user.email,
+      picture: req.user.picture,
+    }, "PBEL", { expiresIn: "1d" });
+
+    res.redirect("http://localhost:5173");
   }
 );
 
 
 module.exports = {
-    userRouter
+  userRouter
 }
--- day-32/frontend/src/App.jsx
+++ day-32/frontend/src/App.jsx
@@ -1,16 +1,61 @@
 import { useState } from 'react'
 import './App.css'
+import { useEffect } from 'react';
 
 function App() {
   const [count, setCount] = useState(0);
+  const [user, setUser] = useState(null);
+
+
   const handleGoogleLogin = ()=>{
     window.location.href = "http://localhost:8000/api/google/"
   }
 
+  const handleLogout = ()=>{
+    localStorage.removeItem("authToken")
+      
+    window.location.href = "http://localhost:5173/"
+  }
+
+  useEffect(()=>{
+    const queryParam = new URLSearchParams(window.location.search);
+    const token = queryParam.get("token");
+
+    if(token){
+      localStorage.setItem("authToken", token);
+
+      const payloadBase64 = token.split(".")[1];
+
+      const decodedUser = JSON.parse(atob(payloadBase64));
+      setUser(decodedUser);
+    }else{
+      const savedToken = localStorage.getItem("authToken");
+      if(savedToken){
+        const payloadBase64 = savedToken.split(".")[1];
+        setUser(JSON.parse(atob(payloadBase64)))
+      }
+    }
+  }, [])
+
+  // if(!user){
+  //   return <h2>Loading user data...</h2>
+  // }
+
   return (
     <>
+      <div style={{textAlign:"center", marginTop:"60px"}}>
+        <img src={user?.picture} alt="Profile_picture" style={{borderRadius:"50%", width:"100px"}}/>
+        <h1>Welcome, {user?.name}</h1>
+        <p>Email:- {user?.email}</p>
+      </div>
+
+
       <button onClick={handleGoogleLogin}>
         Sign in with Google
+      </button>
+
+      <button onClick={handleLogout}>
+        Logout
       </button>
     </>
   )
